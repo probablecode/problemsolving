0916df38adad1b39008660db2fdd979f2e42ef9b
oxcart <61695415+probablecode@users.noreply.github.com>
2023-11-28 19:04:38 +0900

    [level unrated] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,17 @@
+function solution(numer1, denom1, numer2, denom2) {
+    var answer = [];
+    var numer = numer1 * denom2 + numer2 * denom1;
+    var denom = denom1 * denom2;
+    
+    let small = Math.min(numer, denom);
+    let big = Math.max(numer,denom);    
+    while (small != 0)
+    {
+        const tmp = small;
+        small = big - Math.floor(big / small) * small;
+        big = tmp;
+    }
+    answer.push(numer / big);
+    answer.push(denom / big);
+    return answer;        
+}
