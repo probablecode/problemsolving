7ba41085b7dbd187e5b9f2c48632e5d5a5059d95
oxcart <61695415+probablecode@users.noreply.github.com>
2023-12-28 22:19:00 +0900

    [level unrated] Title: 문자열 섞기, Time: 0.09 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        int len = str1.length();
+        StringBuffer sb = new StringBuffer(len * 2);
+        for (int i = 0; i < len; i++)
+        {
+            sb.append(str1.charAt(i));
+            sb.append(str2.charAt(i));
+        }      
+        return sb.toString();
+        }
+}
