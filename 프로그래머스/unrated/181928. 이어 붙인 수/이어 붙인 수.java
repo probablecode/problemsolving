a543e8c76e6789d71f49f7f9a26835f02ac4b91c
oxcart <61695415+probablecode@users.noreply.github.com>
2023-12-29 22:44:19 +0900

    [level unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int odd = 0;
+        int even = 0;
+        for (int num : num_list)
+        {
+            if (num % 2 == 0)
+                even = even * 10 + num;
+            else
+                odd = odd * 10 + num;
+        }
+        return (even + odd);
+    }
+}
