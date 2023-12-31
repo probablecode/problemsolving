017e1900cc05ae14c7ce86323b4220a08d77a171
oxcart <61695415+probablecode@users.noreply.github.com>
2023-12-31 19:21:31 +0900

    [level unrated] Title: 카운트 업, Time: 0.01 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num - start_num + 1];
+        for (int i = 0, j = start_num; j <= end_num; i++, j++)
+            answer[i] = j;
+        return answer;
+    }
+}
