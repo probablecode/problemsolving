a93fe37d0da0c81a90357be157ecb41a154afe06
oxcart <61695415+probablecode@users.noreply.github.com>
2024-01-04 20:57:14 +0900

    [level unrated] Title: 문자열 정수의 합, Time: 0.05 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String num_str) {
+        int sum = 0;
+        for (char ch : num_str.toCharArray())
+            sum += (ch - '0');
+        return sum;
+    }
+}
