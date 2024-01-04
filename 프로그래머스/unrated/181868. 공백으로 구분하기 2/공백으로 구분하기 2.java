f21c2fb33e7bd64270bede3d313a238ebb1665a9
oxcart <61695415+probablecode@users.noreply.github.com>
2024-01-04 21:41:07 +0900

    [level unrated] Title: 공백으로 구분하기 2, Time: 2.22 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public String[] solution(String my_string) {
+        return Arrays.stream(my_string.split(" "))
+                       .filter(str -> !str.isEmpty())
+                       .toArray(String[]::new);
+    }
+}
