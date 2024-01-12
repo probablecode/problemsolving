a39edfcc5c8a2ef630d9e7c8ea46e029fccbd1d8
oxcart <61695415+probablecode@users.noreply.github.com>
2024-01-13 04:47:50 +0900

    [level Lv.0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/Lv.0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,5 +1,3 @@
 function solution(a, b) {
-    strA = a.toString();
-    strB = b.toString();
-    return Math.max(Number(`${strA}${strB}`), Number(`${strB}${strA}`));
+    return Math.max(Number(`${a}${b}`), Number(`${b}${a}`));
 }
