5bb4e9307560086419bac0d18106f3815d2e631b
oxcart <61695415+probablecode@users.noreply.github.com>
2024-01-13 06:44:45 +0900

    [level Lv.0] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,11 @@
+function solution(ineq, eq, n, m) {
+    if (ineq === ">" && eq === "=")
+        return n >= m ? 1 : 0;
+    if (ineq === ">" && eq === "!")
+        return n > m ? 1 : 0;
+    if (ineq === "<" && eq === "=")
+        return n <= m ? 1 : 0;
+    if (ineq === "<" && eq === "!")
+        return n < m ? 1 : 0;
+    return 0;
+}
