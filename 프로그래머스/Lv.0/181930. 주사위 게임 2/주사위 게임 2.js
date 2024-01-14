59be9e871fe876b2f65dc48d6afcf53ab8711983
oxcart <61695415+probablecode@users.noreply.github.com>
2024-01-14 23:29:02 +0900

    [level Lv.0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/Lv.0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,12 +1,12 @@
 function solution(a, b, c) {
-    var p = a === b;
-    var q = b === c;
-    var r = c === a;
+    var p = a !== b;
+    var q = b !== c;
+    var r = c !== a;
     var answer = (a + b + c);
-    if (!p && !q && !r)
+    if (p && q && r)
         return answer;
     answer *= (a ** 2 + b ** 2 + c ** 2);
-    if (!p || !q || !r)
+    if (p || q || r)
         return answer;
     answer *= (a ** 3 + b ** 3 + c ** 3);
     return answer;
