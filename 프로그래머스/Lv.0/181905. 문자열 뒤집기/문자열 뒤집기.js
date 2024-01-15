cc461434d5d1b0b93248fdabc679a5e3aa31fbe3
oxcart <61695415+probablecode@users.noreply.github.com>
2024-01-16 00:55:07 +0900

    [level Lv.0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181905. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/Lv.0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -3,6 +3,5 @@ function solution(my_string, s, e) {
     var reversed = my_string.substring(s, e + 1).split('').reverse();
     var str = my_string.split('');
     str.splice(s, e - s + 1, ...reversed);
-    console.log(str);
     return str.join('');
 }
