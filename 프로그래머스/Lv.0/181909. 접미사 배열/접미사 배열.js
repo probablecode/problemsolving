faea511011a23b8c0c1faf817106c05cc350a03b
oxcart <61695415+probablecode@users.noreply.github.com>
2024-01-16 00:38:18 +0900

    [level Lv.0] Title: 접미사 배열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/Lv.0/181909. 접미사 배열/접미사 배열.js
@@ -1,7 +1,7 @@
 function solution(my_string) {
     var answer = [];
-    for (var i = my_string.length - 1; 0 <= i; i--) {
-        answer.push(my_string.substring(i, my_string.length));
+    for (var i = 0; i < my_string.length; i++) {
+        answer.push(my_string.substring(i));
     }
     return answer.sort();
 }
