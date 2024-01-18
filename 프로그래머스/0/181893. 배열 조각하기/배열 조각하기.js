d0afb899b6c3c8c6a9b5bb56333dc08c01102c85
oxcart <61695415+probablecode@users.noreply.github.com>
2024-01-18 23:32:30 +0900

    [level 0] Title: 배열 조각하기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -0,0 +1,9 @@
+function solution(arr, query) {
+    query.forEach((v, i) => {
+        if (i % 2 === 0)
+            arr.splice(v + 1);
+        else
+            arr.splice(0, v);
+    });
+    return arr;
+}
