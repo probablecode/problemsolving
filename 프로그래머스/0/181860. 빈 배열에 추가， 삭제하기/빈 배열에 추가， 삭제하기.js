e59e2eb00a48cf15e50475ffcd13bf5ec492b1a5
oxcart <61695415+probablecode@users.noreply.github.com>
2024-01-22 21:09:49 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
@@ -2,8 +2,7 @@ function solution(arr, flag) {
     var answer = [];
     for (var i = 0; i < flag.length; i++) {
         if (flag[i])
-            for (var j = 0; j < arr[i] * 2; j++)
-                answer.push(arr[i]);
+            answer.push(... new Array(arr[i] * 2).fill(arr[i]));
         else
             answer.splice(-arr[i]);
     }
