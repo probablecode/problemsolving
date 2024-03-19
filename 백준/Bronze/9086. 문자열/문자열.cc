a114110657f3e5ea6d4d1b581c9e0efeaada0f00
oxcart <61695415+probablecode@users.noreply.github.com>
2024-03-20 03:38:17 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int    main(void) {
+    int n;
+    string s;
+    
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        cout << s[0] << s[s.length() - 1] << endl;
+    }
+    return 0;
+}
