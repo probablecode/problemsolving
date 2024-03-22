1ee7ad97e2c46714c997f1da3054638e2df59e0b
oxcart <61695415+probablecode@users.noreply.github.com>
2024-03-22 18:43:19 +0900

    [level 2] Title: 소수 찾기, Time: 25.81 ms, Memory: 5.46 MB -BaekjoonHub

--- C++/프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ C++/프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,50 +1,46 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <unordered_set>
-#include <iostream>
+#define MAX 10000000
 using namespace std;
-
-int isprime(int num) {
-    if (num < 3)
-        return (num == 2);
-    int sqrt = 2;
-    while (sqrt * sqrt < num)
-        sqrt++;
-    if (num % 2 == 0)
-        return 0;
-    for (int div = 3; div <= sqrt; div += 2) {
-        if (num % div == 0)
-            return 0;
+enum Prime {
+    Prime,
+    NonPrime,
+};
+void get_primes(vector<bool>& primes) {
+    primes[0] = NonPrime;
+    primes[1] = NonPrime;
+    for (int i = 2; i * i < MAX; ++i) {
+        if (primes[i]) {
+            continue;
+        }
+        for (int j = i * i; j < MAX; j += i) {
+            primes[j] = NonPrime;
+        }
     }
-    return 1;
 }
-
+void back_tracking(const vector<bool>& primes, string& numbers, vector<bool>& visit,int& ret, int& answer) {
+    for (int i = 0; i < numbers.length(); ++i) {
+        if (numbers[i] == '-') {
+            continue;
+        }
+        ret = ret * 10 + (numbers[i] - '0');
+        if (primes[ret] == Prime && !visit[ret]) {
+            visit[ret] = true;
+            answer += 1;
+        }
+        char tmp = numbers[i];
+        numbers[i] = '-';
+        back_tracking(primes, numbers, visit, ret, answer);
+        numbers[i] = tmp;
+        ret /= 10;
+    }
+}
 int solution(string numbers) {
     int answer = 0;
-    auto len = numbers.length();
-    vector<string> nums;
-    unordered_set<int> us;
-    for (int i = 0; i < len; i++)
-        nums.push_back(string(1, numbers[i]));
-    sort(nums.begin(), nums.end());
-    
-    for (int i = 1; i < (1 << len); ++i) {
-        vector<string> comb;
-        for (int j = 0; j < len; ++j) {
-            if (i & (1 << j))
-                comb.push_back(nums[j]);
-        }
-        do {
-            string str = "";
-            for (int i = 0; i < comb.size(); i++)
-                str += comb[i];
-            int num = stoi(str);
-            if (us.find(num) == us.end()) {
-                us.insert(num);
-                answer += isprime(num);
-            }
-        } while (next_permutation(comb.begin(), comb.end()));
-    }
+    int ret = 0;
+    vector<bool> primes(MAX);
+    vector<bool> visit(MAX);
+    get_primes(primes);
+    back_tracking(primes, numbers, visit, ret, answer);
     return answer;
 }
