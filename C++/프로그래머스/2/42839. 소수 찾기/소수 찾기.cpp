9a82ae922b459b4531f1304d97f9cfe0bd1147a3
oxcart <61695415+probablecode@users.noreply.github.com>
2024-03-22 18:02:53 +0900

    [level 2] Title: 소수 찾기, Time: 0.06 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ C++/프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -8,11 +8,12 @@ using namespace std;
 int isprime(int num) {
     if (num < 3)
         return (num == 2);
-    
     int sqrt = 2;
     while (sqrt * sqrt < num)
         sqrt++;
-    for (int div = 2; div <= sqrt; div++) {
+    if (num % 2 == 0)
+        return 0;
+    for (int div = 3; div <= sqrt; div += 2) {
         if (num % div == 0)
             return 0;
     }
@@ -27,24 +28,17 @@ int solution(string numbers) {
     for (int i = 0; i < len; i++)
         nums.push_back(string(1, numbers[i]));
     sort(nums.begin(), nums.end());
-    cout << "nums : \t";
-    for (auto it = nums.begin(); it != nums.end(); it++)
-        cout << *it;
-    cout <<endl;
+    
     for (int i = 1; i < (1 << len); ++i) {
         vector<string> comb;
-        for (int j = 0; j < len; ++j)
+        for (int j = 0; j < len; ++j) {
             if (i & (1 << j))
                 comb.push_back(nums[j]);
-             cout << "comb : \t";
-            for (auto it = comb.begin(); it != comb.end(); it++)
-                cout << *it;
-            cout <<endl;
+        }
         do {
             string str = "";
             for (int i = 0; i < comb.size(); i++)
                 str += comb[i];
-            cout << str << endl;
             int num = stoi(str);
             if (us.find(num) == us.end()) {
                 us.insert(num);
