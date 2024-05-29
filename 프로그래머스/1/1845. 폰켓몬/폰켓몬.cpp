7085e7937b1b307b3af26add37d7349dd69f8c8d
oxcart <61695415+probablecode@users.noreply.github.com>
2024-05-30 01:35:56 +0900

    [level 1] Title: 폰켓몬, Time: 0.18 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,13 +1,11 @@
+
 #include <vector>
 #include <unordered_set>
 using namespace std;
-
-int solution(vector<int> nums)
-{
-    int nHalf = nums.size() / 2;
-    unordered_set<int> unique;
-    for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++)
-        unique.insert(*it);
-    int uniq = unique.size();
-    return uniq < nHalf ? uniq : nHalf;
+int solution(vector<int> nums){
+    unordered_set<int> us;   
+    for (auto it = nums.begin(); it != nums.end(); it++)
+        us.insert(*it);
+    auto ticket = nums.size() / 2;
+    return us.size() < ticket ? us.size() : ticket;
 }
