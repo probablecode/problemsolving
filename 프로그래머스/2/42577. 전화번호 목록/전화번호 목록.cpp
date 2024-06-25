50c455d2c186e1db10d3320725cddbb00c88154a
oxcart <61695415+probablecode@users.noreply.github.com>
2024-06-25 09:39:31 +0900

    [level 2] Title: 전화번호 목록, Time: 132.93 ms, Memory: 49.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -5,16 +5,42 @@
 
 using namespace std;
 
+#include <iostream>
+struct Compare {
+    bool operator () (const int a, const int b) {
+        return a < b;
+    }
+    
+};
+
+
 bool solution(vector<string> pb) {
     set<string> s;
-    for (auto it = pb.begin(); it != pb.end(); it++)
+    
+    for (auto it = pb.begin(); it != pb.end(); it++) {
         s.insert(*it);
-    for (auto it = s.begin();;) {
-         auto tmp = it++;
-         if (it == s.end())
-             return true;
-         else if (it->find(*tmp) == 0)
-             break;
     }
-    return false;
+    auto it = s.begin();
+    while (true) {
+        auto pre = it++;
+        if (it == s.end())
+            break;
+        else if (it->find(*pre) == 0)
+            return false;
+    }
+    return true;
 }
+
+// bool solution(vector<string> pb) {
+//     set<string> s;
+//     for (auto it = pb.begin(); it != pb.end(); it++)
+//         s.insert(*it);
+//     for (auto it = s.begin();;) {
+//          auto tmp = it++;
+//          if (it == s.end())
+//              return true;
+//          else if (it->find(*tmp) == 0)
+//              break;
+//     }
+//     return false;
+// }
