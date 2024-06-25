a30ba795bb8930544dc89e835fd4a9002297bcde
oxcart <61695415+probablecode@users.noreply.github.com>
2024-06-25 10:45:35 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -4,11 +4,57 @@
 using namespace std;
 
 int solution(vector<vector<string>> cl) {
-    unordered_map<string, int> ums;
-    for (auto it = cl.begin(); it != cl.end(); it++)
-        ums[(*it)[1]] += 1;
-    int ret = 1;
-    for (auto it = ums.begin(); it != ums.end(); it++)
-        ret *= (it->second + 1);
-    return ret - 1;
+    unordered_map<string, int> um;
+    int answer = 1;
+    for (auto it = cl.begin(); it != cl.end(); it++) {
+        auto tag = (*it)[1];
+        if (0 < um.count(tag))
+            um[tag]++;
+        else
+            um[tag] = 1;    
+    }
+    for (auto it = um.begin(); it != um.end(); it++) {
+        answer *= (it->second + 1);
+    }
+    return answer - 1;
 }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //////////////////////////////////////////
+// int solution(vector<vector<string>> cl) {
+//     unordered_map<string, int> ums;
+//     for (auto it = cl.begin(); it != cl.end(); it++)
+//         ums[(*it)[1]] += 1;
+//     int ret = 1;
+//     for (auto it = ums.begin(); it != ums.end(); it++)
+//         ret *= (it->second + 1);
+//     return ret - 1;
+// }
