21493a6348f27343afe16b4f2fcceec886a18afb
oxcart <61695415+probablecode@users.noreply.github.com>
2024-06-26 16:53:32 +0900

    [level 3] Title: 네트워크, Time: 0.04 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,39 +1,31 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
-int visited[200] = {0, };
-int remain = 0;
-int N = 0;
 
-void trav(int idx, queue<int> &q, vector<vector<int>> &mtx) {
-    for (int i = 0; i < N; i++) {
-        if (mtx[idx][i] && visited[i] == 0) {
-            visited[i] = 1;
-            remain--;
-            q.push(i);
+void clearNet(vector<vector<int>> &mtx, const int n, const int idx) {
+    vector<int> toClear;
+    for (int j = 0; j < n; j++) {
+        if (mtx[idx][j] == 1) {
+            toClear.push_back(j);
+            mtx[idx][j] = 0;
+    
         }
+    }
+    for (auto it = toClear.begin(); it != toClear.end(); it++) {
+        clearNet(mtx, n, *it);
     }
 }
 
-int solution(int n, vector<vector<int>> mtx) {
-    N = n;
-    remain = n;
+int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    queue<int> q;
-    while (0 < remain) {
-        answer++;
-        int idx = 0;
-        while (idx < n && visited[idx])
-            idx++;
-        visited[idx] = 1;
-        remain--;
-        q.push(idx);
-        while (!q.empty()) {
-            int next = q.front();
-            q.pop();
-            trav(next, q, mtx);
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (computers[i][j] == 1) {
+                clearNet(computers, n, i);
+                answer++;
+            }
         }
     }
     return answer;
