0160f62ffb7a19c9bd4803983ab10d8f5aa452b2
oxcart <61695415+probablecode@users.noreply.github.com>
2024-08-22 20:32:21 +0900

    [Silver III] Title: N과 M (3), Time: 352 ms, Memory: 47000 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 public class Main {
     public static int N;
     public static int M;
-    public static int[] num;
 
     public static BufferedWriter bw;
+    public static char[] charArray;
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
-        num = new int[M];
+        charArray = ("  ".repeat(M - 1) + " \n").toCharArray();
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
         go(0);
         bw.close();
@@ -22,24 +22,13 @@ public class Main {
 
     private static void go(int len) throws IOException {
         if (len == M) {
-            print();
+            bw.write(new String(charArray));
             return;
         }
         for (int i = 0; i < N; i++) {
-            num[len] = i + 1;
+            charArray[len * 2] = (char)('1' + i);
             go(len + 1);
         }
     }
 
-    private static void print() throws IOException {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            sb.append(num[i]);
-            if (i < M - 1) {
-                sb.append(' ');
-            }
-        }
-        sb.append('\n');
-        bw.write(sb.toString());
-    }
 }
