a4e7f6b4dd87d08cbfa79096249f09d06ff30dcf
oxcart <61695415+probablecode@users.noreply.github.com>
2024-08-25 15:56:27 +0900

    [Silver II] Title: N과 M (12), Time: 184 ms, Memory: 18136 KB -BaekjoonHub

---
+++ Java/백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,55 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+    public static int N;
+    public static int M;
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int[] count;
+    public static String[] num;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String[] N_M = sc.nextLine().split(" ");
+        N = Integer.parseInt(N_M[0]);
+        M = Integer.parseInt(N_M[1]);
+
+        TreeSet<Integer> tSet = new TreeSet<>();
+        String[] split = sc.nextLine().split(" ");
+        for (int i = 0; i < N; i++)
+            tSet.add(Integer.parseInt(split[i]));
+        N = tSet.size();
+        int k = 0;
+        num = new String[N];
+        for (int n : tSet)
+            num[k++] = String.valueOf(n) + " ";
+        count = new int[N];
+        go(0,0);
+        bw.close();
+    }
+
+    private static void go(int base, int len) throws IOException {
+        if (len == M) {
+            print();
+            return;
+        }
+
+        for (int i = base; i < N; i++) {
+            count[i]++;
+            go(i, len + 1);
+            count[i]--;
+        }
+    }
+
+    private static void print() throws IOException {
+        StringBuilder sb = new StringBuilder(64);
+
+        for (int i = 0; i < N; i++)
+            if (0 < count[i])
+                sb.append(num[i].repeat(count[i]));
+        sb.setCharAt(sb.length() - 1, '\n');
+        bw.write(sb.toString());
+    }
+}
