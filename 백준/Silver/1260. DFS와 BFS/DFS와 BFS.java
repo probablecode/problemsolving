08d9528030cbbfd49a241fc6e3037afb6e0c56f9
oxcart <61695415+probablecode@users.noreply.github.com>
2024-09-24 22:32:59 +0900

    [Silver II] Title: DFS와 BFS, Time: 412 ms, Memory: 25100 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,69 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-
     public static ArrayList<TreeSet<Integer>> adjList;
-    public static boolean[] checked;
+    public static int N;
+    public static int M;
+    public static boolean[] check;
+    public static List<Integer> bfsList = new ArrayList<>();
+    public static List<Integer> dfsList = new ArrayList<>();
 
-    public static List<Integer> dfsList = new ArrayList<Integer>();
-    public static List<Integer> bfsList = new ArrayList<Integer>();
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int V = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] N_M_V = br.readLine().split(" ");
+        N = Integer.parseInt(N_M_V[0]);
         adjList = new ArrayList<>(N + 1);
-        for (int i = 0; i <= N; i++) {
+        for (int i = 0; i <= N; i++)
             adjList.add(new TreeSet<>());
-        }
+        M = Integer.parseInt(N_M_V[1]);
         for (int i = 0; i < M; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            adjList.get(a).add(b);
-            adjList.get(b).add(a);
+            String[] from_to = br.readLine().split(" ");
+            int from = Integer.parseInt(from_to[0]);
+            int to = Integer.parseInt(from_to[1]);
+            adjList.get(from).add(to);
+            adjList.get(to).add(from);
         }
+        br.close();
+        int V = Integer.parseInt(N_M_V[2]);
 
-
-        checked = new boolean[N + 1];
+        check = new boolean[N + 1];
+        check[V] = true;
         dfs(V);
-
-        checked = new boolean[N + 1];
-        Queue<Integer> bfsQueue = new LinkedList<>();
-        bfsQueue.add(V);
-        checked[V] = true;
-        while (!bfsQueue.isEmpty()) {
-            Integer poll = bfsQueue.poll();
+        printQueue(dfsList);
+        
+        Queue<Integer> bsfQ = new LinkedList<>();
+        check = new boolean[N + 1];
+        check[V] = true;
+        bsfQ.add(V);
+        while (!bsfQ.isEmpty()) {
+            int poll = bsfQ.poll();
             bfsList.add(poll);
-
             for (int to : adjList.get(poll)) {
-                if (checked[to]) {
-                    continue;
+                if (!check[to]) {
+                    bsfQ.add(to);
+                    check[to] = true;
                 }
-                bfsQueue.add(to);
-                checked[to] = true;
             }
         }
+        printQueue(bfsList);
+    }
 
-        System.out.print(V);
-        for (int i = 1; i < dfsList.size(); i++)
-            System.out.printf(" %d", dfsList.get(i));
-        System.out.println();
-        System.out.print(V);
-
-        for (int i = 1; i < bfsList.size(); i++)
-            System.out.printf(" %d", bfsList.get(i));
+    private static void printQueue(List<Integer> queue) {
+        System.out.print(queue.get(0));
+        for (int i = 1; i < queue.size(); i++) {
+            System.out.printf(" %d", queue.get(i));
+        }
         System.out.println();
     }
 
     private static void dfs(int v) {
-        if (checked[v])
-            return;
         dfsList.add(v);
-        checked[v] = true;
-
-        for (int to :  adjList.get(v))
+        for (int to : adjList.get(v)){
+            if (check[to])
+                continue;
+            check[to] = true;
             dfs(to);
+        }
     }
 }
