7219ba7a96a25c050355d2ef315185665b0f7f91
oxcart <61695415+probablecode@users.noreply.github.com>
2024-10-02 19:29:15 +0900

    [Gold V] Title: 평범한 배낭, Time: 196 ms, Memory: 53800 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,12 +1,11 @@
 import java.io.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] N_K = br.readLine().split(" ");
         int N = Integer.parseInt(N_K[0]);
         int K = Integer.parseInt(N_K[1]);
-        int[][] memo = new int[K + 1][N + 1];
-
         int[] W = new int[N + 1];
         int[] V = new int[N + 1];
         for (int i = 1; i <= N; i++) {
@@ -14,15 +13,22 @@ public class Main {
             W[i] = Integer.parseInt(W_V[0]);
             V[i] = Integer.parseInt(W_V[1]);
         }
-
-        for (int k = 1; k <= K; k++) {
-            for (int n = 1; n <= N; n++) {
-                int preK = k - W[n];
-                int changed = (preK < 0) ? 0 : memo[preK][n - 1] + V[n];
-                memo[k][n] = Math.max(changed, memo[k][n - 1]);
+        
+        int[][] m = new int[N + 1][K + 1];
+        
+        
+        for (int j = 1; j <= K; j++) {
+            for (int i = 1; i <= N; i++) {
+                int sum = m[i - 1][j];
+                if (W[i] <= j)
+                    sum = Math.max(sum, m[i - 1][j - W[i]] + V[i]);
+                m[i][j] = sum;
             }
         }
-        System.out.println(memo[K][N]);
+        System.out.println(m[N][K]);
+        
+     
+  
         br.close();
     }
 }
